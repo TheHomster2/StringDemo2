490989f517aee8cd68cb57c64f482b86e336433c
srafi1 <shakilrafi0@gmail.com>
2017-10-04 20:41:40 -0400

    done

--- stringy.c
+++ stringy.c
@@ -3,6 +3,7 @@
 
 int strlen2(char *s);
 char* strcpy2(char *dest, char *src);
+char* strncpy2(char *dest, char *src, int n);
 char* strcat2(char *dest, char *src);
 int strcmp2(char *s1, char *s2);
 char* strchr2(char *s, char c);
@@ -18,7 +19,7 @@ int main() {
 
     printf("s1: %s\n", s1);
     printf("s2: %s\n", s2);
-    printf("s3: %s\n", s3);
+    printf("s3: %s\n\n", s3);
 
     printf("Testing strlen(s2)\n");
     printf("standard: %d\n", (int) strlen(s2));
@@ -30,11 +31,33 @@ int main() {
     strcpy2(mys1, mys2);
     printf("mine: %s\n", mys1);
 
+    printf("Testing strncpy(s3, s2, 3)\n");
+    strncpy(s3, s2, 3);
+    printf("standard: %s\n", s3);
+    strncpy2(mys3, mys2, 3);
+    printf("mine: %s\n", mys3);
+
+
     printf("Testing strcat(s1, s3)\n");
     strcat(s1, s2);
     printf("standard: %s\n", s1);
     strcat2(mys1, mys2);
     printf("mine: %s\n", mys1);
+
+    printf("Testing strcmp(s1, s3)\n");
+    printf("Comparing ab to abc\n");
+    printf("standard: %d\n", strcmp("ab", "abc"));
+    printf("mine: %d\n", strcmp2("ab", "abc"));
+    printf("Comparing abc to ab\n");
+    printf("standard: %d\n", strcmp("abc", "ab"));
+    printf("mine: %d\n", strcmp2("abc", "ab"));
+    printf("Comparing ab to ab\n");
+    printf("standard: %d\n", strcmp("ab", "ab"));
+    printf("mine: %d\n", strcmp2("ab", "ab"));
+
+    printf("Testing strchr(s1, 'z')\n");
+    printf("standard: %p\n", strchr("abcz", 'z'));
+    printf("mine: %p\n", strchr2("abcz", 'z'));
 
     return 0;
 }
@@ -57,6 +80,15 @@ char* strcpy2(char *dest, char *src) {
     return dest;
 }
 
+char* strncpy2(char *dest, char *src, int n) {
+    int count = 0;
+    while (src[count] && count < n) {
+        dest[count] = src[count];
+        count++;
+    }
+    return dest;
+}
+
 char* strcat2(char *dest, char *src){
 	int count = 0;
 	int count2;
@@ -70,4 +102,20 @@ char* strcat2(char *dest, char *src){
 	dest[count] = 0;
 	return dest;
 }
-	
+
+int strcmp2(char *s1, char *s2) {
+    int index = 0;
+    while (s1[index] == s2[index] && s1[index]) {
+        //printf("currently: %c and %c\n", s1[index], s2[index]);
+        index++;
+    }
+    return s1[index] - s2[index];
+}
+
+char* strchr2(char *s, char c) {
+    int index = 0;
+    while (s[index] != c && &s) {
+        index++;
+    }
+    return s + index;
+}
